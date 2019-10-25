272d0d26b5aa52dbaa48333d0fce54292a42452d
Anne Fausto <a.r.fausto@gmail.com>
2019-10-25 21:52:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,11 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student)
-    
+    end 
   end
 
   def add_student_attributes(attributes_hash)
+    
     
   end
 
