8199909d1511964cfad3be10f84287f02fa01325
Anne Fausto <a.r.fausto@gmail.com>
2019-10-25 21:43:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
     student_cards. each do |card|
       students << {
         :name => card.css('div.card-text-container h4.student-name').text
-        :location =>         <p class="student-location">Scottsdale, AZ</p>
+        :location => card.css class="student-location">Scottsdale, AZ</p>
               </div>')
       }
   end
