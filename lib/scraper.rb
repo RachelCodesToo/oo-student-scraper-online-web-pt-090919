a49db426665183ec93ccbf6b5916deaea2f9c5ec
Anne Fausto <a.r.fausto@gmail.com>
2019-10-25 21:41:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,22 @@
 require 'open-uri'
 require 'pry'
 
-class Scraper
+html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/)
+doc = Nokogiri::HTML(html)
 
-  def self.scrape_index_page(index_url)
+
+class Scraper
+  
     
+  def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    student_cards = doc.css("div.roster-cards-container .student_card")
+    students = []
+    
+    student_cards. each do |card|
+      students << {
+        :name => card.css
+      }
   end
 
   def self.scrape_profile_page(profile_url)
