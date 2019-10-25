1543a7bfaf2db59c97518273827e07aaf1feef4a
Anne Fausto <a.r.fausto@gmail.com>
2019-10-25 21:55:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,8 +23,8 @@ class Scraper
     doc = Nokogiri::HTML(open(profile_url))
     
     social_media_links = doc.css('div.social-icon-container a')
-    profile_quote = doc.css("div.vitals-text-container .profile-quote").text
-    biography = doc.css("div.description-holder p").text
+    profile_quote = doc.css('div.vitals-text-container .profile-quote').text
+    biography = doc.css('div.description-holder p').text
     
     social = {}
     
